0324b77255dc62f6608263abca4cf8dd1edfe923
ebina4yaka <4yaka3bina@gmail.com>
2021-01-06 11:53:00 +0900

    Add email column

--- backend/db/migrate/20210106003247_create_users.rb
+++ backend/db/migrate/20210106003247_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
+      t.string :email, null: false
       t.string :user_id, null: false, unique: true
       t.string :password_digest, null: false
       t.boolean :activated, null: false, default: false
