6930df89b7c271d749a2b733cd3baaf5b956d604
ebina4yaka <4yaka3bina@gmail.com>
2021-01-06 14:30:46 +0900

    Add user model validation tests

--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -10,4 +10,8 @@ class ActiveSupport::TestCase
   end
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
+
+  def active_user
+    User.find_by(activated: true)
+  end
 end
