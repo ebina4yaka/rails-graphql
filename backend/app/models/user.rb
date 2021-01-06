0eb787bcfb38d29476f859d5d672e0ab6feb88e0
ebina4yaka <4yaka3bina@gmail.com>
2021-01-06 10:50:50 +0900

    Add users validates

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  # bcrypt
+  has_secure_password
+  # validates
+  validates :name, presence: true,
+            length: { maximum: 30, allow_blank: true }
+
+  validates :user_id, presence: true,
+            uniqueness: true
+
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+            length: { minimum: 8 },
+            format: {
+              with: VALID_PASSWORD_REGEX
+            },
+            allow_blank: true
 end
