7d682ce21454eb42d9ba2dda2b04fd8eb378c356
ebina4yaka <4yaka3bina@gmail.com>
2021-01-21 16:54:28 +0900

    Create mutation

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -1,5 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
+    field :unfollow_user, mutation: Mutations::UnfollowUser
+    field :follow_user, mutation: Mutations::FollowUser
     field :create_post, mutation: Mutations::CreatePost
     field :create_user, mutation: Mutations::CreateUser
   end
