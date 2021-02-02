7fb4a15e639253dde9fd675f25a3b63de6b49db9
ebina4yaka <4yaka3bina@gmail.com>
2021-02-02 14:42:40 +0900

    Fixed failure to issue query when followers count is 0

--- backend/app/graphql/loaders/user_followers_count_loader.rb
+++ backend/app/graphql/loaders/user_followers_count_loader.rb
@@ -1,7 +1,9 @@
 module Loaders
   class UserFollowersCountLoader < GraphQL::Batch::Loader
     def perform(user_ids)
-      FollowsRelationship.where(follow_id: user_ids).group(:follow_id).count.each do |follow_id, count|
+      User.joins('left outer join follows_relationships f on users.id = f.follow_id')
+          .where(id: user_ids).group(:id)
+          .count(:follow_id).each do |follow_id, count|
         fulfill(follow_id, count)
       end
     end
